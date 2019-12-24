ff385e55b4111cb767067a77037b4b08a0c79c52
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2019-12-23 22:36:02 -0700

    creating global variables

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var row1 = document.getElementById("row1")
-var row2 = document.getElementById("row2")
-var row2 = document.getElementById("row3")
-var row2 = document.getElementById("row4")
-var row2 = document.getElementById("row5")
-var row2 = document.getElementById("row6")
-var row2 = document.getElementById("row7")
-var row2 = document.getElementById("row8")
+var saveBtn = document.getElementById("saveBtn")
+var startHour = document.getElementById("row1")
+var endHour = document.getElementById("row9")
+
