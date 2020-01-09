3612b95058c120c77d215e418b486f08e94d73e8
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-01-08 20:31:28 -0700

    changes to html

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-var saveBtn = document.getElementById("saveBtn")
-var startHour = document.getElementById("row1")
-var endHour = document.getElementById("row9")
+// var saveBtn = document.getElementById("saveBtn")
+// var startHour = document.getElementById("row1")
+// var endHour = document.getElementById("row9")
 
+// var today = new Date();
+// var dd = String(today.getDate()).padStart(2, '0');
+// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+// var yyyy = today.getFullYear();
+
+// today = mm + '/' + dd + '/' + yyyy;
+// document.write(today);
+
+let date = document.getElementById("date");
+let output = document.getElementById("date");
+
+function date(){
+    let today = new Date() + 1;
+    let  month = today.getMonth();
+    let year = today.getFullYear();
+    let date = today.getDate();
+
+    let currentDate = `$(month)/$(date)/$(year)`;
+    output.innerText = currentDate;
+
+}
